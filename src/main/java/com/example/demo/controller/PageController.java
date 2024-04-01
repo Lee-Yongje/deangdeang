c961b36ccdb30500a7bffc79ff373f9f52681173
jusunKim <155524612+jusunKim@users.noreply.github.com>
2024-04-01 12:16:42 +0900

    카카오 api 환경변수 설정 관련 추가 [주선]

--- src/main/java/com/example/demo/controller/PageController.java
+++ src/main/java/com/example/demo/controller/PageController.java
@@ -1,6 +1,8 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -191,15 +193,20 @@ public class PageController {
     //-사진형게시판
     
     //병원
+    @Value("${kakao.api.key}")
+    private String kakaoApiKey;
+    
     @GetMapping("/hospital")
     public String hospitalPage() {
     	return "hospital";
     }
     @GetMapping("/hospitalDetail")
-    public String hospitalDetailPage() {
+    public String hospitalDetailPage(Model model) {
+    	model.addAttribute("kakaoApiKey", kakaoApiKey);
     	return "hospitalDetail";
     }
     //-병원
+    
     @GetMapping("/diaryDetail")
     public String diaryDetailPage() {
     	return "diaryDetail";
