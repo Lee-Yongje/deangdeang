f7167a344f8bf26c0729720bffb1e8011e323e70
jusunKim <155524612+jusunKim@users.noreply.github.com>
2024-04-03 11:19:45 +0900

    Community, Diary, Mypage, Region, Usedgood Controller 생성 [주선]

---
+++ src/main/java/com/example/demo/controller/UsedgoodController.java
@@ -0,0 +1,17 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.example.demo.dto.LoginFormDTO;
+
+@Controller
+public class UsedgoodController {
+
+    
+    
+}
+    
