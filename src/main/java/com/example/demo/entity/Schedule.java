651831ba09a368702f367eee5e27f31d5c6c5aaa
King0-0 <King0-0>
2024-04-15 17:37:10 +0900

    다이어리, 스케줄러 수정[수영]

--- src/main/java/com/example/demo/entity/Schedule.java
+++ src/main/java/com/example/demo/entity/Schedule.java
@@ -1,11 +1,6 @@
 package com.example.demo.entity;
 
 import java.sql.Date;
-import java.time.LocalDateTime;
-
-import org.apache.catalina.User;
-import org.springframework.web.multipart.MultipartFile;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
@@ -13,26 +8,21 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Data;
 
-
 @Data
 @Entity
 @Table(name="schedule")
 public class Schedule {
-	
-	@Id
-	private int sno;
-	
-	private Date s_date;
-	private String s_content;
-	private String s_complete;
-	
-	@ManyToOne
-	@JoinColumn(name="uno")
-	private Users users;
-	
-	@ManyToOne
-	@JoinColumn(name="pno")
-	private Puppy puppy;
-	
+    @Id
+    private int sno;
+    private Date s_date;
+    private String s_content;
+    private String s_complete;
 
+    @ManyToOne
+    @JoinColumn(name="uno")
+    private Users users;
+
+    @ManyToOne
+    @JoinColumn(name="pno")
+    private Puppy puppy;
 }
