bb6360be54929f7830e300a5049f46d1d29a0463
jusunKim <155524612+jusunKim@users.noreply.github.com>
2024-04-18 10:55:18 +0900

    pull을 위한 push. github최종본과 동일함을 확인 [주선]

--- src/test/java/com/example/demo/FinalApplicationTests.java
+++ src/test/java/com/example/demo/FinalApplicationTests.java
@@ -1,18 +1,13 @@
-package com.example.demo;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-import com.example.demo.dao.UserRepository;
-import com.example.demo.entity.Users;
-
-@SpringBootTest
-class FinalApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-}
+//package com.example.demo;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class FinalApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
