bef94ed6bfbf575fa19673c3edc0a8e9cb8f0323
jusunKim <155524612+jusunKim@users.noreply.github.com>
2024-04-23 16:35:22 +0900

    TimeZone 설정 [주선]

--- src/main/java/com/example/demo/FinalApplication.java
+++ src/main/java/com/example/demo/FinalApplication.java
@@ -1,17 +1,26 @@
 package com.example.demo;
 
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import jakarta.annotation.PostConstruct;
+
 @SpringBootApplication
 public class FinalApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(FinalApplication.class, args);
 	}
-
+	
+	@PostConstruct
+	public void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
+	
 	@Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
